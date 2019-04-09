cf58a0faa18ae3a3f3a7908fe207af35b76a4ccd
yarntime <yarntime@163.com>
2019-04-09 14:58:21 +0800

    support import cli flags from other package

---
+++ flagsflag.go
@@ -0,0 +1,22 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+)
+
+type FlagsFlag struct {
+	*flag.Flag
+}
+
+func (f FlagsFlag) Apply(flag *flag.FlagSet) {
+	flag.Var(f.Flag.Value, f.Name, f.Usage)
+}
+
+func (f FlagsFlag) GetName() string {
+	return f.Name
+}
+
+func (f FlagsFlag) String() string {
+	return fmt.Sprintf("%s \"%v\"\t%v", prefixedNames(f.Flag.Name, ""), f.Flag.Value, f.Flag.Usage)
+}
